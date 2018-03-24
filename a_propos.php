f6e31ca41db90c68a84b2f86370eb21583b64fe6
Alyndel <37742421+Alyndel@users.noreply.github.com>
2018-03-24 16:52:11 +0100

    correction photo index

--- a_propos.php
+++ a_propos.php
@@ -7,7 +7,7 @@
 		<p id="paragraphe-centré">
 			Ce site a été réalisé grâce à Bootstrap.
 			<br><br>
-			<a href="https://goo.gl/NgDLW5">Le lien de l'image de fond :</a>
+			<a href="https://goo.gl/NgDLW5">Le lien de l'image de fond</a>
 		</p>
   </body>
 </html>
