7399865c283004809e3c2a520610273b84d8d44a
Alyndel <37742421+Alyndel@users.noreply.github.com>
2018-03-24 16:48:58 +0100

    paragraphe et hyperlien

--- a_propos.php
+++ a_propos.php
@@ -4,7 +4,7 @@
 
 	<img src="img/back.jpg" id="back-image">
 
-		<p>
+		<p id="paragraphe-centré">
 			Ce site a été réalisé grâce à Bootstrap.
 
 			Le lien de l'image de fond : <a>https://goo.gl/NgDLW5</a>
