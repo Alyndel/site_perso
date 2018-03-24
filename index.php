36f7e8d98747c897c6397d9001670b208648ae68
Hugo NITARD <hugo@vps518370.ovh.net>
2018-03-24 15:40:17 +0100

    v0.5

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+	include 'header.php';
+ ?>
+
+	<img src="img/photo.jpg" id="photo-profil">
+	<img src="img/back.jpg" id="back-image">
+	<p>
+		<br>
+		NITARD Hugo
+		<br><br>
+		BTS Systèmes Informatiques aux organisations, 2ème année
+		<br><br>
+		Option SLAM (Solutions Logicielles et Applications Métiers)
+		<br><br>
+		👍 Développement Web
+	</p>
+</body>
+
+</html>
