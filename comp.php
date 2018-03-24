1f7da350c1c880b978cd09aa73b39cb2a7c122bd
Alyndel <37742421+Alyndel@users.noreply.github.com>
2018-03-24 18:16:30 +0100

    test compétence

--- comp.php
+++ comp.php
@@ -1,8 +1,10 @@
 <?php
 	include 'header.php';
  ?>
+		<img src="img/back.jpg" id="back-image">
 
-		COMPETENCES, OUI !
+		<button type="button" class="btn btn-lg btn-danger" data-toggle="popover" title="Conception d'un applicatif et export de données vers un SGBD PostreSQL pour Novae Aerospace" data-content="A1.1.1 , Analyse du cahier des charges d'un service à produire\nA1.2.5 , Définition des niveaux d'habilitation associés à un service\n">Base de données Novae Aerospace</button>
+
 
   </body>
 </html>
