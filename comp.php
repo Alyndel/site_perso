12f48dca16e21ad891739f8076d550ed6106c572
Alyndel <37742421+Alyndel@users.noreply.github.com>
2018-03-24 18:21:55 +0100

    test carte

--- comp.php
+++ comp.php
@@ -2,8 +2,16 @@
 	include 'header.php';
  ?>
 		<img src="img/back.jpg" id="back-image">
-		<p>
-			<button type="button" class="btn btn-lg btn-danger" data-toggle="popover" title="Conception d'un applicatif et export de données vers un SGBD PostreSQL pour Novae Aerospace" data-content="A1.1.1 , Analyse du cahier des charges d'un service à produire\nA1.2.5 , Définition des niveaux d'habilitation associés à un service\n">Base de données Novae Aerospace</button>
+		<p id="paragraphe-centré">
+			<div class="card" style="width: 100%;">
+			  <div class="card-body">
+			    <h5 class="card-title">Conception d'un applicatif et export de données vers un SGBD PostreSQL pour Novae Aerospace</h5>
+			    <p class="card-text">
+						A1.1.1 , Analyse du cahier des charges d'un service à produire
+						<br>
+					</p>
+  			</div>
+			</div>
 		</p>
 
   </body>
