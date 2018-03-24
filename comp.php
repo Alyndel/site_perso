1201d4a27d1a2e28cfeeabb0d8a85df0b184473c
Alyndel <37742421+Alyndel@users.noreply.github.com>
2018-03-24 18:17:38 +0100

    avec paragraphe

--- comp.php
+++ comp.php
@@ -2,9 +2,9 @@
 	include 'header.php';
  ?>
 		<img src="img/back.jpg" id="back-image">
-
-		<button type="button" class="btn btn-lg btn-danger" data-toggle="popover" title="Conception d'un applicatif et export de données vers un SGBD PostreSQL pour Novae Aerospace" data-content="A1.1.1 , Analyse du cahier des charges d'un service à produire\nA1.2.5 , Définition des niveaux d'habilitation associés à un service\n">Base de données Novae Aerospace</button>
-
+		<p>
+			<button type="button" class="btn btn-lg btn-danger" data-toggle="popover" title="Conception d'un applicatif et export de données vers un SGBD PostreSQL pour Novae Aerospace" data-content="A1.1.1 , Analyse du cahier des charges d'un service à produire\nA1.2.5 , Définition des niveaux d'habilitation associés à un service\n">Base de données Novae Aerospace</button>
+		</p>
 
   </body>
 </html>
