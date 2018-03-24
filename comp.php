fbd06722d18a015be91a9562155b6d5c9b58f460
Alyndel <37742421+Alyndel@users.noreply.github.com>
2018-03-24 18:30:20 +0100

    on met des rem

--- comp.php
+++ comp.php
@@ -3,7 +3,7 @@
  ?>
 		<img src="img/back.jpg" id="back-image">
 		<p id="paragraphe">
-			<div class="card" style="width: 100%;">
+			<div class="card" style="width: 18rem;">
 			  <div class="card-body">
 			    <h5 class="card-title">Conception d'un applicatif et export de données vers un SGBD PostreSQL pour Novae Aerospace</h5>
 			    <p class="card-text">A1.1.1 , Analyse du cahier des charges d'un service à produire </p>
